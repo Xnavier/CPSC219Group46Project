23561d4729a4e62af29ab077b9df032d70a7fff1
Neon Tang Li <l.neontang@gmail.com>
2022-12-05 17:45:51 -0700

    Created transactionlist classes and methods

---
+++ src/app/TransactionList.java
@@ -0,0 +1,40 @@
+package app;
+import java.util.ArrayList;
+
+import app.Payment;
+
+public class TransactionList {
+	private String name;
+	private ArrayList<Payment> paymentList = new ArrayList<Payment>();
+	
+	public TransactionList (String listName) {
+		name = listName;
+		paymentList = new ArrayList<Payment>();
+	}
+	
+	public ArrayList<Payment> getTransactions() {
+		return paymentList;
+	}
+	
+	public String getName() {
+		return name;
+	}		
+	
+	public void addTransaction(Payment payment) {
+		paymentList.add(payment);
+	}
+	
+	public long getTotalAmount() {
+		int length = paymentList.size();
+		int counter = 0;
+		int dollars = 0;
+		int cents = 0;
+		while (counter < length) {
+			counter++;
+			dollars +=  paymentList.get(counter).getDollars();
+			cents += paymentList.get(counter).getCents();
+		}
+		return 100 * dollars + cents;
+	}
+	
+}

b7787add52a847307f1e681f84abec9851682379
JacobOkon <jacob.okon>
2022-12-06 00:45:35 +0000

    Finishing planned expense scene method and income scene method for the controller class. Starting to create final method to add together all the values in the app.

--- src/app/BudgetCalculatorController.java
+++ src/app/BudgetCalculatorController.java
@@ -58,51 +58,49 @@ public class BudgetCalculatorController {
  
 
 	
-  public  double getIncome (String valueEntered) {
-    	
-    	boolean validMoney = true;{
-    		
-    	}
-    	
-    	double finBudget = 0.0;
-    	if (validMoney);
-    		return finBudget = Double.parseDouble(valueEntered);
-    	
-    	
-    }
   
   
   	double planExpense = 0.0;
-  	void calculatePlanExpense (Scene mainScene, TextField planExpenseTextField) {
+	private Object planExpenseLabel;
+  	void calculatePlanExpense (Scene mainScene, Label planExpenseLabel) {
   		appStage.setScene(mainScene);
-  		planExpense = Double.parseDouble(planExpenseTextField.getText());
+  		planExpense = Double.parseDouble(planExpenseLabel.getText());
   	}
 	
 	@FXML
 	void getExpenseScene(ActionEvent enterExpenseEvent) {
     	Scene mainScene = appStage.getScene();
     	
-    	int inputBoxes = 0; 
-    	int numExpenses = 4;
-    	while(inputBoxes<numExpenses) {
+    	int finExpense = 0;
+    	
     	
     	VBox expenseBox = new VBox();
     		
     		HBox foodField = new HBox();
     		
     		Label foodEstimate = new Label("Estimate the value of the food expenses per month.");
-    		TextField foodBox = new TextField();
-    		foodBox.getText();
+    		TextField foodExpenseDollars = new TextField();
+    		TextField foodExpenseCents = new TextField();
+    		
+    		finExpense = finExpense + ((Integer.parseInt(foodExpenseDollars.getText()))+ ((Integer.parseInt(foodExpenseCents.getText())/ 100)));
+    		
+    		HBox utilitiesField = new HBox();
+    		Label utilitiesEstimate = new Label ("Enter the rough amount of money spent per month on utilities.");
+    		TextField utilitiesDollars = new TextField();
+    		TextField utilitiesCents = new TextField();
+    		
+    		finExpense = finExpense + ((Integer.parseInt(utilitiesDollars.getText()))+ ((Integer.parseInt(utilitiesCents.getText())/ 100)));
+    		
     		
     	
-    	Button foodDoneButton = new Button ("Done");
-    	foodDoneButton.setOnAction(doneEvent -> findTotalExpense(mainScene));
+    	Button eDoneButton = new Button ("Done");
+    	eDoneButton.setOnAction(doneEvent -> findTotalExpense(mainScene, planExpenseLabel));
     	
     		
     	
 
 	}
-    	Scene plannedExpenseScene = new Scene(expenseBox);
+ 
     	
     
   
@@ -110,36 +108,56 @@ public class BudgetCalculatorController {
 
 
 
-}
+
 	
+	private Object findTotalExpense(Scene mainScene, Object planExpenseLabel2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+
+
+
+
 	double income = 0.0;
+	private Label incomeLabel;
 	
-	void calculateIncome (Scene mainScene, TextField incomeTextField) {
+	public void calculateIncome (Scene mainScene, Label incomeLabel) {
 		appStage.setScene(mainScene);
-		income = Double.parseDouble(incomeTextField.getText());
+		income += Double.parseDouble(incomeLabel.getText());
 		
 	}
 	@FXML
 	void getIncome (ActionEvent enterIncome) {
 		Scene mainScene = appStage.getScene();
-		double finIncome = 0; 
+		int finIncome = 0; 
 		
 		
 	VBox incomeBox = new VBox();
 		HBox fullTimeField = new HBox();
 		Label incomeDirectionsLabel = new Label("Enter the approximate amount of money you make from your full time job per month.");
-		TextField fullTimeTextField = new TextField();
+		TextField fullTimeDollars = new TextField();
+		TextField fullTimeCents = new TextField();
+		
+		finIncome = finIncome + ((Integer.parseInt(fullTimeDollars.getText()))+ ((Integer.parseInt(fullTimeCents.getText())/ 100)));
 		
 		HBox partTimeField = new HBox();
 		Label partTimeDirectionsLabel = new Label ("enter the amount of money you make from any other streams of income.");
-		TextField partTimeTextField = new TextField();
+		TextField partTimeDollars= new TextField();
+		TextField partTimeCents = new TextField();
+		finIncome = finIncome + ((Integer.parseInt(partTimeDollars.getText()))+ ((Integer.parseInt(partTimeCents.getText())/ 100)));
 		
-		Button ptDoneButton = new Button ("Next");
 		
 		
-		Button ftDoneButton = new Button("Next");
-		ftDoneButton.setOnAction(nextEvent -> calculateIncome(mainScene, incomeTextField) );
-		incomeBox.getChildren().addAll(partTimeField, partTimeDirectionsLabel,partTimeTextField);
+		
+		Button iDoneButton = new Button("Next");
+		iDoneButton.setOnAction(nextEvent -> calculateIncome(mainScene, incomeLabel) );
+		incomeBox.getChildren().addAll(partTimeField, partTimeDirectionsLabel,partTimeDollars, partTimeCents);
 	
 	
 		
